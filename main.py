e6345f599c9f30149be3dd6bd807d239593e9cda
AnilPise7 <anilapise7@gmail.com>
2021-02-04 20:49:17 +0200

    First Commit

---
+++ main.py
@@ -0,0 +1,30 @@
+from flask import Flask, request
+from flashtext import KeywordProcessor
+
+app = Flask(__name__)
+
+def add(extract):
+          keyword_processor = KeywordProcessor()
+          r = keyword_processor.add_keyword(extract)
+          return r
+
+def prediction(text):
+    keyword_processor = KeywordProcessor()
+    keyword_processor.add_keyword('school')
+    keyword_processor.add_keyword('bus')
+    keyword_processor.add_keyword('!@#$%')
+    keywords_found = keyword_processor.extract_keywords(text, span_info=True)
+    return keywords_found
+
+@app.route("/predict", methods=['POST'])
+def predictRoute():
+    data = request.json["text"]
+    p = prediction(data)
+    print(p)
+    print(type(p))
+    return {"Keyword": str(p)}
+
+
+if __name__ == "__main__":
+    #app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=6500, debug=True)
